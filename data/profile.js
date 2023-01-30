4147f815f9dfeaaf2f54ff5a3160efaa8c215004
Toi Drayton <47548569+TDD-Drayton@users.noreply.github.com>
2023-01-30 14:52:38 -0500

    Resume Files Updated

---
+++ data/profile.js
@@ -0,0 +1,134 @@
+const profileData = {
+  title: "CV",
+  name: "Toi Drayton",
+  sub_title: "DevOps Network Engineer",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `I am an innovative and passionate developer with a primary interest in <strong>JavaScript</strong>
+     and it's applications. Experienced in designing and implementing sophisticated UI/UX with
+      Progressive Web Application standards and application life-cycle managements.`,
+    contact: {
+      email: "************@gmail.com",
+      phone: "***.***.****",
+      address: "USA"
+    }
+  },
+  links: [
+    { title: "Github", src: "https://github.com/TDD-Drayton" },
+    { title: "LinkedIn", src: "https://www.linkedin.com/in/toi-drayton-995a3a199/" }
+
+  ],
+  education: [
+    {
+      alma: "Harvard Extension School",
+      duration: "Audit",
+      std: "Bachelor of Liberal Arts",
+      score: "3.0"
+    },
+    {
+      alma: "Eastern Florida State College",
+      duration: "3 Semesters: 2016-2017",
+      std: "Associate of Science",
+      score: "3.0"
+    },
+    {
+      alma: "Duncan U. Fletcher High School",
+      duration: "Graduated: 2011",
+      std: "General Education",
+      score: "3.7"
+    }
+  ],
+  skills: [
+    {
+      category: "Programming Languages",
+      topics: ["Python", "C"]
+    },
+    {
+      category: "Automating device and network functions",
+      topics: ["Junos Automation Stack and DevOps Concepts", "XML/NETCONF", "Data Serialization","Python/PyEZ","Rest API"]
+    },
+    {
+      category: "Compentencies: Technical",
+      topics: ["Excel", "Outlook",
+      "SQL","YAML","JSON","Ansible","Jenkins","Docker","Git","Flask",
+      "HTML5","CSS","JavaScript"]
+    }
+
+  ],
+  projects: [
+    {
+      title: "Project Euler",
+      duration: "Current",
+      link: "https://github.com/TDD-Drayton/project-euler",
+      desc: `Challenging mathematical/computer programming problems.`
+    },
+    {
+      title: "Intensive Reader",
+      duration: "Current",
+      link: "https://github.com/TDD-Drayton/intensiveReader_v1.0",
+      desc: `Active engagement and interaction with texts for focused reading.`
+    }
+  ],
+  experiences: [
+    {
+      organization: "Amazon",
+      title: `IT Engineer | IT Support Associate II`,
+      date: "Nov 2017 - Sep 2022",
+      details: [
+        `Researched and collaborated with engineering teams on the implementation of Cisco and Juniper-Network
+configuration, monitoring, and maintenance of routers and switches`,
+        `Managed DHCP, DNS, IPv4/6 addressing, TCP/IP, MPLS/VPLS, BGP, EIGRP, SNMP, and troubleshooting
+network connections of laptops and desktops using utilities such as ping, traceroute, mtr, and telnet.`,
+        `Reviewed metrics of customer support, uptime, and networking data. Presented information through the
+use of Python-Data Analysis and Data Presentation. SQL, Python-Jupyter Notebook.`,
+        `Maintained the physical functionality of devices: Laptops, Desktops, Thinclients, Printers, and mobile
+devices(scanners, tablets, etc.)`,
+        `Managed procurement management of multiple accounts of assets valued over 500,000USD.`,
+        `Collaborated with vendors for the implementation of assets and technology.`
+      ]
+    },
+    {
+      organization: "Centre de Cultura Contemporània de Barcelona",
+      title: `IT Tourism Specialist | Intern`,
+      date: "August 2014 - January 2015",
+      details: [
+        `Installation and Maintenance of the setup of wireless and wired receiver and transmitter devices in venue
+spaces for live speaking and musical presentation.`,
+        `Partnered with curation executives on artistic installation and maintenance.`,
+        `Promotion as an Ambassador for installations: ‘Big Bang Data’ and ‘Art Flux Film Fest’.`,
+        `Presented information through the use of Python-Data Analysis and Data Presentation. SQL,
+Python-Jupyter Notebook`
+      ]
+    },
+    {
+      organization: "Italki",
+      title: `Community English Tutor | Freelance`,
+      desc: `IELTS Test Preparation`,
+      date: "Since 2015",
+      details: [
+        `Evaluation of exam readiness of students preparing for the IELTS or TOEFL language exams.`,
+        `Development of IELTS and TOEFL study materials for students planning to take the exams.`,
+        `One on One lessons with students.`
+      ]
+    }
+
+  ],
+  certifications: [
+    {
+      desc: `<strong>JNCIA-DevOps</strong> by <strong>Juniper Networks</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.credly.com/users/toi-drayton/badges'>Credly</a>)`,
+      date: "January 2022"
+    },
+    {
+      desc: `<strong>Applied Data Science with Python - Level 2</strong> by <strong>IBM</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.credly.com/users/toi-drayton/badges'>Credly</a>)`,
+      date: "January 2022"
+    },
+    {
+      desc: `<strong>Docker Essentials: A Developer Introduction</strong> by <strong>IBM</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.credly.com/users/toi-drayton/badges'>Credly</a>)`,
+      date: "March 2022"
+    }
+  ],
+  events: []
+};
